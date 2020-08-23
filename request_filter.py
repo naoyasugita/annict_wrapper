a7c1e15488bb837de23239145ccbaea17f277e0f
naoyasugita <sugita6878@gmail.com>
2020-08-23 16:57:53 +0900

    seriesサービスを追加

--- request_filter.py
+++ request_filter.py
@@ -44,3 +44,16 @@ class OrganizationRequestParams:
 
     def to_dict(self):
         return delete_none_for_dict(dataclasses.asdict(self))
+
+
+@dataclasses.dataclass
+class SeriesRequestParams:
+    fields: Optional[str] = None
+    filter_ids: Optional[int] = None
+    filter_name: Optional[int] = None
+    page: Optional[int] = None
+    per_page: Optional[int] = None  # max 50
+    sort_id: Optional[str] = None  # desc or asc
+
+    def to_dict(self):
+        return delete_none_for_dict(dataclasses.asdict(self))
---
+++ service/series.py
@@ -0,0 +1,14 @@
+import dataclasses
+
+from model.series import Series
+from request import ApiRequests
+from request_filter import SeriesRequestParams
+
+
+@dataclasses.dataclass
+class SeriesService:
+    def find_series_info(self, series_id: int) -> Series:
+        api = ApiRequests()
+        params = SeriesRequestParams(filter_ids=series_id).to_dict()
+        res = api.series(params=params)
+        return Series(**res["series"][0])
