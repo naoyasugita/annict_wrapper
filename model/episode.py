7fb7c9b5b6a5593a35cbbf529e89060611d79b59
naoyasugita <sugita6878@gmail.com>
2020-08-23 17:30:32 +0900

    前後のエピソードモデルを作成

--- model/episode.py
+++ model/episode.py
@@ -16,11 +16,43 @@ class Episode:
     records_count: int
     record_comments_count: int
     work: Dict[str, Union[int, str]]
-    prev_episode: Dict[str, Optional[Union[int, str]]]
-    next_episode: Optional[int]
+    prev_episode: Optional[Dict[str, Optional[Union[int, str]]]]
+    next_episode: Optional[Dict[str, Optional[Union[int, str]]]]
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.work = Work(**self.work)
+        if self.prev_episode is not None:
+            self.prev_episode = PrevEpisode(**self.prev_episode)
+        if self.next_episode is not None:
+            self.next_episode = NextEpisode(**self.next_episode)
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class PrevEpisode:
+    id: int
+    number: Optional[int]
+    number_text: str
+    sort_number: int
+    title: str
+    records_count: int
+    record_comments_count: int
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
+
+
+@dataclasses.dataclass
+class NextEpisode:
+    id: int
+    number: Optional[int]
+    number_text: str
+    sort_number: int
+    title: str
+    records_count: int
+    record_comments_count: int
 
     def to_dict(self) -> dict:
         return dataclasses.asdict(self)
