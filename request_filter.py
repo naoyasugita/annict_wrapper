6dc4d25ea807f179350e2fc033756d5827ad3cab
naoyasugita <sugita6878@gmail.com>
2020-08-23 16:03:15 +0900

    リクエスト時のパラメータを管理するクラスを作成

---
+++ request_filter.py
@@ -0,0 +1,33 @@
+import dataclasses
+from typing import Optional
+
+from utils import delete_none_for_dict
+
+
+@dataclasses.dataclass
+class WorkRequestParams:
+    fields: Optional[str] = None
+    filter_ids: Optional[int] = None
+    filter_season: Optional[str] = None
+    filter_title: Optional[str] = None
+    page: Optional[int] = None
+    per_page: Optional[int] = None  # max 50
+    sort_id: Optional[str] = None  # desc or asc
+    sort_season: Optional[str] = None  # desc or asc
+    sort_watchers_count: Optional[str] = None  # desc or asc
+
+    def to_dict(self):
+        return delete_none_for_dict(dataclasses.asdict(self))
+
+
+@dataclasses.dataclass
+class PeopleRequestParams:
+    fields: Optional[str] = None
+    filter_ids: Optional[int] = None
+    filter_name: Optional[int] = None
+    page: Optional[int] = None
+    per_page: Optional[int] = None  # max 50
+    sort_id: Optional[str] = None  # desc or asc
+
+    def to_dict(self):
+        return delete_none_for_dict(dataclasses.asdict(self))
