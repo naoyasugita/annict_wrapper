cb435db3e5d753427f8d74055d2810a21f15d0cb
naoyasugita <sugita6878@gmail.com>
2020-09-03 21:37:10 +0900

    characterモデルのテストを追加

---
+++ tests/model/test_character.py
@@ -0,0 +1,34 @@
+import pytest
+
+from annict_wrapper.model.character import Character
+from annict_wrapper.model.character import Characters
+
+
+class TestCharacterModel:
+    def test_to_dict(self, fixture_character):
+        character_dict = fixture_character["character_dict"]
+        assert Character(**character_dict).to_dict() == character_dict
+
+
+class TestCharactersModel:
+    def test_characters_append_when_type_ok(
+        self, fixture_character, fixture_character_2
+    ):
+        characters = Characters()
+        character = fixture_character["character"]
+        character_2 = fixture_character_2["character"]
+        characters.append(character)
+        characters.append(character_2)
+        assert len(characters._list) == 2
+
+    def test_characters_append_when_type_error(self):
+        characters = Characters()
+        with pytest.raises(TypeError):
+            characters.append("dammy")
+
+    def test_characters_to_dict(self, fixture_character):
+        characters = Characters()
+        character = fixture_character["character"]
+        character_dict = fixture_character["character_dict"]
+        characters.append(character)
+        assert characters.to_dict() == [character_dict]
