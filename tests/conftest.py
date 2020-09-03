a043f117f56f1696b80107f794ebe531dcfc1e3b
naoyasugita <sugita6878@gmail.com>
2020-09-03 23:34:22 +0900

    fixture内部でfixtureを呼び出すように修正

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,193 +7,22 @@ from annict_wrapper.model.episode import NextEpisode
 from annict_wrapper.model.episode import PrevEpisode
 from annict_wrapper.model.organization import Organization
 from annict_wrapper.model.people import People
+from annict_wrapper.model.program import Program
+from annict_wrapper.model.series import Series
+from annict_wrapper.model.staff import Staff
+from annict_wrapper.model.work import Work
 
 
 @pytest.fixture
-def fixture_cast():
+def fixture_cast(fixture_work, fixture_character, fixture_people):
     cast_dict = {
         "id": 12345,
         "name": "テスト太郎",
         "name_en": "Test, Taro",
         "sort_number": 1,
-        "work": {
-            "id": 1234,
-            "title": "テストワーク",
-            "title_kana": "てすとわーく",
-            "media": "tv",
-            "media_text": "TV",
-            "released_on": "",
-            "released_on_about": "",
-            "official_site_url": "http://testhoeghoge.com",
-            "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
-            "twitter_username": "test_hogehoge",
-            "twitter_hashtag": "テスト",
-            "syobocal_tid": "1234",
-            "mal_anime_id": "12345",
-            "images": {
-                "recommended_url": "test.jpg",
-                "facebook": {"og_image_url": "test.jpg"},
-                "twitter": {
-                    "mini_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=mini",
-                    "normal_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=normal",
-                    "bigger_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=bigger",
-                    "original_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=original",
-                    "image_url": "",
-                },
-            },
-            "episodes_count": 12,
-            "watchers_count": 1234,
-            "reviews_count": 11,
-            "no_episodes": False,
-            "season_name": "2018-winter",
-            "season_name_text": "2018年冬",
-        },
-        "character": {
-            "id": 12345,
-            "name": "テストキャラ",
-            "name_kana": "てすときゃら",
-            "name_en": "Test, Chara",
-            "nickname": "",
-            "nickname_en": "",
-            "birthday": "",
-            "birthday_en": "",
-            "age": "",
-            "age_en": "",
-            "blood_type": "",
-            "blood_type_en": "",
-            "height": "",
-            "height_en": "",
-            "weight": "",
-            "weight_en": "",
-            "nationality": "",
-            "nationality_en": "",
-            "occupation": "",
-            "occupation_en": "",
-            "description": "",
-            "description_en": "",
-            "description_source": "",
-            "description_source_en": "",
-            "favorite_characters_count": 10,
-            "series": None,
-            "kind": "",
-        },
-        "person": {
-            "id": 1234,
-            "name": "テストパーソン",
-            "name_kana": "てすとぱーそん",
-            "name_en": "Test, Person",
-            "nickname": "てすとん",
-            "nickname_en": "",
-            "gender_text": "女性",
-            "url": "http://hogehoge.html",
-            "url_en": "",
-            "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
-            "wikipedia_url_en": "",
-            "twitter_username": "",
-            "twitter_username_en": "",
-            "birthday": "2000-01-01",
-            "blood_type": "a",
-            "height": 150,
-            "favorite_people_count": 11,
-            "casts_count": 115,
-            "staffs_count": 0,
-            "prefecture": None,
-        },
-    }
-    cast = Cast(**cast_dict)
-    yield {"cast": cast, "cast_dict": cast_dict}
-
-
-@pytest.fixture
-def fixture_cast_2():
-    cast_dict = {
-        "id": 123456,
-        "name": "テスト太郎2",
-        "name_en": "Test, Taro2",
-        "sort_number": 1,
-        "work": {
-            "id": 1234,
-            "title": "テストワーク",
-            "title_kana": "てすとわーく",
-            "media": "tv",
-            "media_text": "TV",
-            "released_on": "",
-            "released_on_about": "",
-            "official_site_url": "http://testhoeghoge.com",
-            "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
-            "twitter_username": "test_hogehoge",
-            "twitter_hashtag": "テスト",
-            "syobocal_tid": "1234",
-            "mal_anime_id": "12345",
-            "images": {
-                "recommended_url": "test.jpg",
-                "facebook": {"og_image_url": "test.jpg"},
-                "twitter": {
-                    "mini_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=mini",
-                    "normal_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=normal",
-                    "bigger_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=bigger",
-                    "original_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=original",
-                    "image_url": "",
-                },
-            },
-            "episodes_count": 12,
-            "watchers_count": 1234,
-            "reviews_count": 11,
-            "no_episodes": False,
-            "season_name": "2018-winter",
-            "season_name_text": "2018年冬",
-        },
-        "character": {
-            "id": 123456,
-            "name": "テストキャラ2",
-            "name_kana": "てすときゃら",
-            "name_en": "Test, Chara",
-            "nickname": "",
-            "nickname_en": "",
-            "birthday": "",
-            "birthday_en": "",
-            "age": "",
-            "age_en": "",
-            "blood_type": "",
-            "blood_type_en": "",
-            "height": "",
-            "height_en": "",
-            "weight": "",
-            "weight_en": "",
-            "nationality": "",
-            "nationality_en": "",
-            "occupation": "",
-            "occupation_en": "",
-            "description": "",
-            "description_en": "",
-            "description_source": "",
-            "description_source_en": "",
-            "favorite_characters_count": 10,
-            "series": {"id": 123, "name": "Steins;Gate", "name_ro": "", "name_en": "",},
-            "kind": "",
-        },
-        "person": {
-            "id": 1234,
-            "name": "テストパーソン",
-            "name_kana": "てすとぱーそん",
-            "name_en": "Test, Person",
-            "nickname": "てすとん",
-            "nickname_en": "",
-            "gender_text": "女性",
-            "url": "http://hogehoge.html",
-            "url_en": "",
-            "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
-            "wikipedia_url_en": "",
-            "twitter_username": "",
-            "twitter_username_en": "",
-            "birthday": "2000-01-01",
-            "blood_type": "a",
-            "height": 150,
-            "favorite_people_count": 11,
-            "casts_count": 115,
-            "staffs_count": 0,
-            "prefecture": {"id": 1, "name": "東京都"},
-        },
+        "work": fixture_work["work_dict"],
+        "character": fixture_character["character_dict"],
+        "person": fixture_people["people_dict"],
     }
     cast = Cast(**cast_dict)
     yield {"cast": cast, "cast_dict": cast_dict}
@@ -235,42 +64,7 @@ def fixture_character():
 
 
 @pytest.fixture
-def fixture_character_2():
-    character_dict = {
-        "id": 12345,
-        "name": "テスト太郎2",
-        "name_kana": "テストたろう2",
-        "name_en": "Test, Taro2",
-        "kind": "HogeHoge2",
-        "nickname": "テストタロウ2",
-        "nickname_en": "Test, Taro2",
-        "birthday": "12月1日",
-        "birthday_en": "December 1",
-        "age": "18歳",
-        "age_en": "18",
-        "blood_type": "A型",
-        "blood_type_en": "A",
-        "height": "177 cm",
-        "height_en": "177 cm",
-        "weight": "59 kg",
-        "weight_en": "59 kg",
-        "nationality": "日本",
-        "nationality_en": "Japan",
-        "occupation": "テストテストテストテスト",
-        "occupation_en": "test test test test",
-        "description": "サンプル",
-        "description_en": "",
-        "description_source": " てスト",
-        "description_source_en": "",
-        "favorite_characters_count": 123,
-        "series": {"id": 12, "name": "Steins;Gate", "name_ro": "", "name_en": ""},
-    }
-    character = Character(**character_dict)
-    yield {"character": character, "character_dict": character_dict}
-
-
-@pytest.fixture
-def fixture_episode():
+def fixture_episode(fixture_work, fixture_prev_episode, fixture_next_episode):
     episode_dict = {
         "id": 45,
         "number": None,
@@ -279,40 +73,9 @@ def fixture_episode():
         "title": "test1",
         "records_count": 0,
         "record_comments_count": 0,
-        "work": {
-            "id": 1234,
-            "title": "テストワーク",
-            "title_kana": "てすとわーく",
-            "media": "tv",
-            "media_text": "TV",
-            "released_on": "",
-            "released_on_about": "",
-            "official_site_url": "http://testhoeghoge.com",
-            "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
-            "twitter_username": "test_hogehoge",
-            "twitter_hashtag": "テスト",
-            "syobocal_tid": "1234",
-            "mal_anime_id": "12345",
-            "images": {
-                "recommended_url": "test.jpg",
-                "facebook": {"og_image_url": "test.jpg"},
-                "twitter": {
-                    "mini_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=mini",
-                    "normal_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=normal",
-                    "bigger_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=bigger",
-                    "original_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=original",
-                    "image_url": "",
-                },
-            },
-            "episodes_count": 12,
-            "watchers_count": 1234,
-            "reviews_count": 11,
-            "no_episodes": False,
-            "season_name": "2018-winter",
-            "season_name_text": "2018年冬",
-        },
-        "prev_episode": None,
-        "next_episode": None,
+        "work": fixture_work["work_dict"],
+        "prev_episode": fixture_prev_episode["prev_episode_dict"],
+        "next_episode": fixture_next_episode["next_episode_dict"],
     }
     episode = Episode(**episode_dict)
     yield {"episode": episode, "episode_dict": episode_dict}
@@ -394,3 +157,104 @@ def fixture_people():
     }
     people = People(**people_dict)
     yield {"people": people, "people_dict": people_dict}
+
+
+@pytest.fixture
+def fixture_work():
+    work_dict = {
+        "id": 1234,
+        "title": "テストワーク",
+        "title_kana": "てすとわーく",
+        "media": "tv",
+        "media_text": "TV",
+        "released_on": "",
+        "released_on_about": "",
+        "official_site_url": "http://testhoeghoge.com",
+        "wikipedia_url": "http://hogehoge/%E6%9D%B1%E5%B1%B1%E5%A5%88%E5%A4%AE",
+        "twitter_username": "test_hogehoge",
+        "twitter_hashtag": "テスト",
+        "syobocal_tid": "1234",
+        "mal_anime_id": "12345",
+        "images": {
+            "recommended_url": "test.jpg",
+            "facebook": {"og_image_url": "test.jpg"},
+            "twitter": {
+                "mini_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=mini",
+                "normal_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=normal",
+                "bigger_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=bigger",
+                "original_avatar_url": "https://twitter.com/xxxxxxxxxxx/profile_image?size=original",
+                "image_url": "",
+            },
+        },
+        "episodes_count": 12,
+        "watchers_count": 1234,
+        "reviews_count": 11,
+        "no_episodes": False,
+        "season_name": "2018-winter",
+        "season_name_text": "2018年冬",
+    }
+
+    work = Work(**work_dict)
+    yield {"work": work, "work_dict": work_dict}
+
+
+@pytest.fixture
+def fixture_program(fixture_work, fixture_episode):
+    program_dict = {
+        "id": 35387,
+        "started_at": "2016-05-07T20:10:00.000Z",
+        "is_rebroadcast": False,
+        "channel": {"id": 4, "name": "日本テレビ"},
+        "work": fixture_work["work_dict"],
+        "episode": fixture_episode["episode_dict"],
+    }
+    program = Program(**program_dict)
+    yield {"program": program, "program_dict": program_dict}
+
+
+@pytest.fixture
+def fixture_series():
+    series_dict = {
+        "id": 12,
+        "name": "テスト",
+        "name_ro": "test",
+        "name_en": "test",
+    }
+    series = Series(**series_dict)
+    yield {"series": series, "series_dict": series_dict}
+
+
+@pytest.fixture
+def fixture_staff_when_org(fixture_work, fixture_organization):
+    staff_dict = {
+        "id": 35319,
+        "name": "京都アニメーション",
+        "name_en": "",
+        "role_text": "アニメーション制作",
+        "role_other": "",
+        "role_other_en": "",
+        "sort_number": 200,
+        "work": fixture_work["work_dict"],
+        "organization": fixture_organization["organization_dict"],
+        "person": None,
+    }
+    staff = Staff(**staff_dict)
+    yield {"staff": staff, "staff_dict": staff_dict}
+
+
+@pytest.fixture
+def fixture_staff_when_person(fixture_work, fixture_people):
+    staff_dict = {
+        "id": 35319,
+        "name": "京都アニメーション",
+        "name_en": "",
+        "role_text": "アニメーション制作",
+        "role_other": "",
+        "role_other_en": "",
+        "sort_number": 200,
+        "work": fixture_work["work_dict"],
+        "organization": None,
+        "person": fixture_people["people_dict"],
+    }
+    staff = Staff(**staff_dict)
+    yield {"staff": staff, "staff_dict": staff_dict}
