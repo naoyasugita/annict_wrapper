f5173ea9d42c2705a6b83aaed729f8362e819dd6
naoyasugita <sugita6878@gmail.com>
2020-09-05 22:46:16 +0900

    TestCheckDateFormatの追加

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 import pytest
 
 from annict_wrapper.model.work import Cool
+from annict_wrapper.utils import check_date_format
 from annict_wrapper.utils import create_season_by_year_and_cool
 from annict_wrapper.utils import delete_none_for_dict
 
@@ -35,4 +38,38 @@ class TestCreateSeasonByYearAndCool:
         cool = Cool["spring"]
         actual = str(year) + "-" + cool.name
         expected = create_season_by_year_and_cool(year, cool)
+        assert actual == expected
+
+
+class TestCheckDateFormat:
+    def test_check_date_format(self):
+        dt = "2016/05/06 21:10"
+        excepted = check_date_format(dt)
+
+        actual = True
+
+        assert actual == excepted
+
+    def test_check_date_format_when_missed_format_1(self):
+        dt = "2016-05-06 21:10"
+        expected = check_date_format(dt)
+
+        actual = False
+
+        assert actual == expected
+
+    def test_check_date_format_when_missed_format_2(self):
+        dt = "2016/05/06 21:10:12"
+        expected = check_date_format(dt)
+
+        actual = False
+
+        assert actual == expected
+
+    def test_check_date_format_when_missed_format_3(self):
+        dt = "2016年05月06日 21時10分"
+        expected = check_date_format(dt)
+
+        actual = False
+
         assert actual == expected
