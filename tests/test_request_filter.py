249d6b92ed057799e22e806b58c5be1c6c5a1b30
naoyasugita <sugita6878@gmail.com>
2020-09-05 20:21:46 +0900

    PeopleRequestParamsのテストを追加

--- tests/test_request_filter.py
+++ tests/test_request_filter.py
@@ -1,3 +1,4 @@
+from annict_wrapper.request_filter import PeopleRequestParams
 from annict_wrapper.request_filter import WorkRequestParams
 
 
@@ -29,3 +30,30 @@ class TestWorkRequestParams:
         params = {}
         work_request_params = WorkRequestParams(**params)
         assert work_request_params.to_dict() == params
+
+
+class TestPeopleRequestParams:
+    def test_to_dict_not_in_none(self):
+        params = {
+            "fields": "id",
+            "filter_ids": 123,
+            "filter_name": "井上",
+            "page": 2,
+            "per_page": 30,
+            "sort_id": "desc",
+        }
+        people_request_params = PeopleRequestParams(**params)
+        assert people_request_params.to_dict() == params
+
+    def test_to_dict_in_none(self):
+        params = {
+            "fields": "id",
+            "filter_ids": 123,
+        }
+        people_request_params = PeopleRequestParams(**params)
+        assert people_request_params.to_dict() == params
+
+    def test_to_dict_when_empty(self):
+        params = {}
+        people_request_params = PeopleRequestParams(**params)
+        assert people_request_params.to_dict() == params
