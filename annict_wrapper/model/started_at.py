6bd30374a097fb47e31889b72b2f265cbb563b41
naoyasugita <sugita6878@gmail.com>
2021-01-08 23:47:55 +0900

    微修正

--- annict_wrapper/model/started_at.py
+++ annict_wrapper/model/started_at.py
@@ -9,4 +9,6 @@ class StartedAt:
 
     def __post_init__(self):
         if not check_date_format(self.dt):
-            raise ValueError(f"dt in missed format. correct format is %Y/%m/%d %H:%M.")
+            raise ValueError(
+                f"{self.dt} in missed format. correct format is %Y/%m/%d %H:%M."
+            )
