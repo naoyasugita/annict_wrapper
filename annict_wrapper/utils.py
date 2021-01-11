34cd39813ed17f7248fba510ab151ec1bbfa34dc
naoyasugita <sugita6878@gmail.com>
2021-01-11 22:37:16 +0900

    バリデーションのの追加

--- annict_wrapper/utils.py
+++ annict_wrapper/utils.py
@@ -1,4 +1,10 @@
 from datetime import datetime
+from typing import Any
+from typing import Type
+from typing import TypeVar
+from typing import cast
+
+import dateutil.parser
 
 from annict_wrapper.model.work import Cool
 
@@ -21,3 +27,29 @@ def check_date_format(d: str) -> bool:
         return True
     except ValueError:
         return False
+
+T = TypeVar("T")
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def from_datetime(x: Any) -> datetime:
+    return dateutil.parser.parse(x)
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
+def from_bool(x: Any) -> bool:
+    assert isinstance(x, bool)
+    return x
