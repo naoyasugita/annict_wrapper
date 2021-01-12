1068348841f98d180d2a76552b9ee3e0d9a20c30
naoyasugita <sugita6878@gmail.com>
2021-01-12 23:45:31 +0900

    型チェックのテスト追加

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,17 @@
 from datetime import datetime
 
 import pytest
-
+from annict_wrapper.model.series import Series
 from annict_wrapper.model.work import Cool
 from annict_wrapper.utils import check_date_format
 from annict_wrapper.utils import create_season_by_year_and_cool
 from annict_wrapper.utils import delete_none_for_dict
+from annict_wrapper.utils import from_bool
+from annict_wrapper.utils import from_datetime
+from annict_wrapper.utils import from_int
+from annict_wrapper.utils import from_none
+from annict_wrapper.utils import from_str
+from annict_wrapper.utils import to_class
 
 
 class TestDeleteNoneForDict:
@@ -85,3 +91,96 @@ class TestCheckDateFormat:
         actual = False
 
         assert actual == expected
+
+
+
+class TestTypeChecker:
+    def test_from_int_when_int(self):
+        actual = 1
+        expected = from_int(1)
+
+        assert actual == expected
+
+    def test_from_int_when_str(self):
+        with pytest.raises(AssertionError):
+            expected = from_int("1")
+
+    def test_from_int_when_bool(self):
+        with pytest.raises(AssertionError):
+            expected = from_int(True)
+
+
+    def test_from_str_when_str(self):
+        actual = "test"
+        expected = from_str("test")
+
+        assert actual == expected
+
+    def test_from_str_when_int(self):
+        with pytest.raises(AssertionError):
+            expected = from_str(1)
+
+    def test_from_datetime_when_datetime_1(self):
+        """ YYYY-MM-DD hh:mm """
+        actual = datetime(2016, 5, 6, 21, 10)
+        expected = from_datetime("2016-05-06 21:10")
+
+        assert actual == expected
+
+    def test_from_datetime_when_datetime_2(self):
+        """ YYYYMMDDhhmmss """
+        actual = datetime(2020, 10, 10, 11, 11, 22)
+        expected = from_datetime("20201010111122")
+
+        assert actual == expected
+
+    def test_from_datetime_when_datetime_3(self):
+        """ YYYYMMDDhhmmss """
+        actual = datetime(2020, 10, 10, 11, 11, 22)
+        expected = from_datetime("20201010111122")
+
+        assert actual == expected
+
+    def test_from_datetime_when_int(self):
+        """ YYYYMMDDhhmmss """
+        with pytest.raises(TypeError):
+            expected = from_datetime(123)
+
+    def test_from_bool_when_bool(self):
+        actual = True
+        expected = from_bool(True)
+
+        assert actual == expected
+
+    def test_from_bool_when_int(self):
+        with pytest.raises(AssertionError):
+            expected = from_bool(1)
+
+    def test_from_bool_when_str(self):
+        with pytest.raises(AssertionError):
+            expected = from_bool("True")
+
+    def test_from_none_when_none(self):
+        actual = None
+        expected = from_none(None)
+
+        assert actual == expected
+
+    def test_from_none_when_int(self):
+        with pytest.raises(AssertionError):
+            expected = from_none(1)
+
+    def test_to_class_when_class(self, fixture_series):
+        actual = fixture_series["series_dict"]
+        series = fixture_series["series"]
+        expected = to_class(Series, series)
+
+        assert actual == expected
+
+    def test_to_class_when_not_class(self, fixture_series, fixture_work):
+        actual = fixture_work["work_dict"]
+        work = fixture_work["work"]
+        with pytest.raises(AssertionError):
+            expected = to_class(Series, work)
+
+
