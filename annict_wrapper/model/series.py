d2db72a7f31fb70595ab8fd27af30320d5d35048
naoyasugita <sugita6878@gmail.com>
2021-01-14 22:56:22 +0900

    seriesモデルの修正

--- annict_wrapper/model/series.py
+++ annict_wrapper/model/series.py
@@ -20,16 +20,6 @@ class SeriesId:
 
 
 @dataclasses.dataclass(frozen=True)
-class Name:
-    """ 名前 """
-
-    value: str
-
-    def __post_init__(self) -> None:
-        from_str(self.value)
-
-
-@dataclasses.dataclass(frozen=True)
 class NameRo:
     """ 名前 (ローマ字表記) """
 
@@ -49,26 +39,40 @@ class NameEn:
         from_str(self.value)
 
 
+@dataclasses.dataclass(frozen=True)
+class Name:
+    """ 名前 """
+
+    value: str
+    roma: NameRo
+    english: NameEn
+
+    def __post_init__(self) -> None:
+        from_str(self.value)
+        assert isinstance(self.roma, NameRo)
+        assert isinstance(self.english, NameEn)
+
+
 @dataclasses.dataclass
 class Series:
     series_id: SeriesId
     name: Name
-    name_ro: NameRo
-    name_en: NameEn
 
     def to_dict(self) -> dict:
         return {
             "id": dataclasses.asdict(self.series_id)["value"],
             "name": dataclasses.asdict(self.name)["value"],
-            "name_ro": dataclasses.asdict(self.name_ro)["value"],
-            "name_en": dataclasses.asdict(self.name_en)["value"],
+            "name_ro": dataclasses.asdict(self.name.roma)["value"],
+            "name_en": dataclasses.asdict(self.name.english)["value"],
         }
 
     @staticmethod
     def from_dict(series_dict: dict) -> "Series":
         return Series(
-            series_id=SeriesId(series_dict["id"]),
-            name=Name(series_dict["name"]),
-            name_ro=NameRo(series_dict["name_ro"]),
-            name_en=NameEn(series_dict["name_en"]),
+            SeriesId(series_dict["id"]),
+            Name(
+                series_dict["name"],
+                NameRo(series_dict["name_ro"]),
+                NameEn(series_dict["name_en"]),
+            ),
         )
--- tests/model/test_staff.py
+++ tests/model/test_staff.py
@@ -8,51 +8,49 @@ class TestStaffModel:
         actual = fixture_staff_when_org["staff_dict"]
         staff = fixture_staff_when_org["staff"]
 
-        print(staff)
-
         excepted = staff.to_dict()
 
         assert actual == excepted
 
-#     def test_from_dict_when_person(self, fixture_staff_when_person):
-#         actual = fixture_staff_when_person["staff"]
+    def test_from_dict_when_person(self, fixture_staff_when_person):
+        actual = fixture_staff_when_person["staff"]
 
-#         staff_dict = fixture_staff_when_person["staff_dict"]
-#         excepted = Staff.from_dict(staff_dict)
+        staff_dict = fixture_staff_when_person["staff_dict"]
+        excepted = Staff.from_dict(staff_dict)
 
-#         assert actual == excepted
+        assert actual == excepted
 
 
-# class TestStaffsModel:
-#     def test_staffs_append_when_type_ok(
-#         self, fixture_staff_when_org, fixture_staff_when_person
-#     ):
-#         staffs = Staffs()
-#         staff = fixture_staff_when_org["staff"]
-#         staff_2 = fixture_staff_when_person["staff"]
-#         staffs.append(staff)
-#         staffs.append(staff_2)
-#         excepted = len(staffs._list)
+class TestStaffsModel:
+    def test_staffs_append_when_type_ok(
+        self, fixture_staff_when_org, fixture_staff_when_person
+    ):
+        staffs = Staffs()
+        staff = fixture_staff_when_org["staff"]
+        staff_2 = fixture_staff_when_person["staff"]
+        staffs.append(staff)
+        staffs.append(staff_2)
+        excepted = len(staffs._list)
 
-#         actual = 2
+        actual = 2
 
-#         assert actual == excepted
+        assert actual == excepted
 
-#     def test_staffs_append_when_type_error(self):
-#         staffs = Staffs()
-#         with pytest.raises(TypeError):
-#             staffs.append("dammy")
+    def test_staffs_append_when_type_error(self):
+        staffs = Staffs()
+        with pytest.raises(TypeError):
+            staffs.append("dammy")
 
-#     def test_staffs_to_dict(self, fixture_staff_when_org, fixture_staff_when_person):
-#         staffs = Staffs()
-#         staff = fixture_staff_when_org["staff"]
-#         staff2 = fixture_staff_when_person["staff"]
-#         staffs.append(staff)
-#         staffs.append(staff2)
-#         excepted = staffs.to_dict()
+    def test_staffs_to_dict(self, fixture_staff_when_org, fixture_staff_when_person):
+        staffs = Staffs()
+        staff = fixture_staff_when_org["staff"]
+        staff2 = fixture_staff_when_person["staff"]
+        staffs.append(staff)
+        staffs.append(staff2)
+        excepted = staffs.to_dict()
 
-#         staff_dict = fixture_staff_when_org["staff_dict"]
-#         staff_dict2 = fixture_staff_when_person["staff_dict"]
-#         actual = [staff_dict, staff_dict2]
+        staff_dict = fixture_staff_when_org["staff_dict"]
+        staff_dict2 = fixture_staff_when_person["staff_dict"]
+        actual = [staff_dict, staff_dict2]
 
-#         assert actual == excepted
+        assert actual == excepted
