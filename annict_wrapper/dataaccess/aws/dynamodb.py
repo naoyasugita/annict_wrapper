7d4a80d864b7e397e35d419c1e36ae020e9dac02
naoyasugita <sugita6878@gmail.com>
2021-01-22 21:55:50 +0900

    wip: Dynamoのクライアントを作成

--- annict_wrapper/dataaccess/aws/dynamodb.py
+++ annict_wrapper/dataaccess/aws/dynamodb.py
@@ -1,26 +1,40 @@
+import dataclasses
+import os
+from typing import List
+from typing import Optional
+
 import boto3
+from tqdm import tqdm
+
+os.environ["AWS_DEFAULT_REGION"] = "ap-northeast-1"
 
 
+@dataclasses.dataclass
 class DynamoDBClient:
     table_name: str
-    table = boto3.resource("dynamodb").Table(self.table_name())
+
+    def __post_init__(self) -> None:
+        self.table = boto3.resource("dynamodb").Table(self.table_name)
 
     def get_item(self, key_name: str, key_value: str) -> Optional[dict]:
         try:
             response = self.table.get_item(Key={key_name: key_value})
-        except ClientError as e:
-            raise (e.response["Error"]["Message"])
+        except Exception as e:
+            print(e)
         else:
-            # return default_proc(response)
             return response
 
     def put_item(self, item: dict) -> None:
         return self.table.put_item(Item=item)
 
-    def batch_weiter(self, items: List[dict]) -> None:
-        with dynamo_table.batch_writer() as batch:
-            for item in items:
-                batch.put_item(Item=item)
+    def batch_writer(self, items: List[dict]) -> None:
+        max_count = len(items)
+        with self.table.batch_writer() as batch:
+            for item in tqdm(items, desc=f"insert {self.table_name}"):
+                try:
+                    batch.put_item(Item=item)
+                except ValidationException as e:
+                    print(f"error[{item['id']}], reason[{e}]")
 
     def delete_item(self, key_name: str, key_value: str) -> None:
         self.table.delete_item(Key={key_name: key_value})
